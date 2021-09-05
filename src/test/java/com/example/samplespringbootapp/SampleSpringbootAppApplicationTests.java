d01d5315f00a794af43b99de6c3aa123141eb0bb
Pandit <pandit@ericsson.com>
2021-09-05 20:54:04 +0530

    sample spring boot app

---
+++ src/test/java/com/example/samplespringbootapp/SampleSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
